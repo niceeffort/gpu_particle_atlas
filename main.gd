9e5dac9f8416544e76ee0a4474331c7db80b5a5e
niceeffort <davidschoemehl@gmail.com>
2025-09-17 09:31:36 -0500

    Find the max 1D texture size and note it in the comment.

--- main.gd
+++ main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 const NUM_UNIQUE_CARDS := 52
-const NUM_CARDS := 1000
+const NUM_CARDS := 1000 # Max of 32768 for a 1D texture
 const IMAGE_SIZE := int(ceil(sqrt(NUM_UNIQUE_CARDS)))
 
 var card_data: Image
@@ -9,6 +9,10 @@ var card_data_texture: ImageTexture
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+
+	# This is how to find the texture size limits for a 1D texture
+	#print("LIMIT_MAX_TEXTURE_SIZE_1D: ",
+	#	RenderingServer.get_rendering_device().limit_get(RenderingDevice.LIMIT_MAX_TEXTURE_SIZE_1D))
 
 	# Create an 2D image to store the id for each card
 	card_data = Image.create(NUM_CARDS, 1, false, Image.FORMAT_RGBAH)
@@ -24,5 +28,7 @@ func _ready():
 	# Set the number of particles to the number of cards
 	$AtlasParticles.amount = NUM_CARDS
 
-func _process(delta):
+
+
+func _process(_delta):
 	get_window().title = str("FPS: %d" % Performance.get_monitor(Performance.TIME_FPS))
